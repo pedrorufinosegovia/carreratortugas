fac02cfe1f2843e4344c7573f749e6b4a0baa0b8
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-04 15:21:29 +0100

    carreraterminada

--- carreradef.py
+++ carreradef.py
@@ -1,9 +1,9 @@
 import turtle
-
+import random 
 class Circuito():
     corredores = []
     __posStrartY = [-30, -10, 10, 30]
-    __colorturtle = ("green", "orange", "black", "blue")
+    __colorturtle = ("green", "red", "black", "blue")
     def __init__(self, width, heigth):
         self.__Screen = turtle.Screen()
         self.__Screen.setup(width, heigth)
@@ -20,7 +20,20 @@ class Circuito():
             newturtle.setpos(self.__startline, self.__posStrartY[i])
             self.corredores.append(newturtle)
             
-
+    def competir(self):
+        hayganador = False
+        velocidades = (10, 15, 20, 25, 30, 35)
+        while not hayganador:
+            for tortuga in self.corredores:
+                avance = velocidades[random.randint(0,5)]
+                tortuga.fd(avance)
+                if tortuga.position()[0] >= self.__finishline:
+                    hayganador = True
+                    print("la tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                    break
+            
+            
 if __name__ == "__main__":
-    circuito = Circuito(640, 480)
+    circuito = Circuito(1000, 480)
+    circuito.competir()
 
