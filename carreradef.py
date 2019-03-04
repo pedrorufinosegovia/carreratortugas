6e6a827038da9a80185dd7a2d3604e24fe3945e1
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-04 12:38:20 +0100

    carrera con circuito y corredores

--- carreradef.py
+++ carreradef.py
@@ -1,2 +1,26 @@
+import turtle
+
 class Circuito():
-    def __init__:(self, corredores[], 
+    corredores = []
+    __posStrartY = [-30, -10, 10, 30]
+    __colorturtle = ("green", "orange", "black", "blue")
+    def __init__(self, width, heigth):
+        self.__Screen = turtle.Screen()
+        self.__Screen.setup(width, heigth)
+        self.__Screen.bgcolor("yellow")
+        self.__startline = -width/2 + 20
+        self.__finishline = width/2 -20
+        self.__createrunners()
+    def __createrunners(self):   
+        for i in range(4):
+            newturtle = turtle.Turtle()
+            newturtle.shape("turtle")
+            newturtle.color(self.__colorturtle[i])
+            newturtle.penup()
+            newturtle.setpos(self.__startline, self.__posStrartY[i])
+            self.corredores.append(newturtle)
+            
+
+if __name__ == "__main__":
+    circuito = Circuito(640, 480)
+
